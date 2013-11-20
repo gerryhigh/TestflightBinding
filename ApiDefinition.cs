e66ddf171045f143004008445344696f53c9f19f
unknown-user <unknown-user@10.227.227.73>
2013-11-20 17:11:26 -0600

    updates for 2.0.2 and binding updates from https://github.com/mono/monotouch-bindings/tree/master/TestFlight

--- ApiDefinition.cs
+++ ApiDefinition.cs
@@ -1,55 +1,103 @@
-using System;
+//
+// testflight.cs: API definition for the TestFlight SDK
+//
+// Authors:
+//  Miguel de Icaza (miguel@xamarin.com)
+//
+// Copyright 2011 Xamarin Inc.
+//
 
-using MonoTouch.ObjCRuntime;
+using System;
 using MonoTouch.Foundation;
-using MonoTouch.UIKit;
 
 namespace TestFlightBinding {
 
 	[BaseType (typeof (NSObject))]
-	public partial interface TestFlight {
-
+	public interface TestFlight {
+		/// <summary>
+		/// Add custom environment information
+		/// If you want to track custom information such as a user name from your application you can add it here
+		/// </summary>
+		/// <param name="information">A string containing the environment you are storing</param>
+		/// <param name="key">The key to store the information with</param>
 		[Static, Export ("addCustomEnvironmentInformation:forKey:")]
 		void AddCustomEnvironmentInformation (string information, string key);
 
+		/// <summary>
+		/// TakeOff with TestFlight
+		/// This methode connects your app with TestFlight
+		/// </summary>
+		/// <param name="applicationToken">Your Application Token can be found on https://testflightapp.com/dashboard/applications/ selecting this application from the list then selecting SDK.</param>
 		[Static, Export ("takeOff:")]
 		void TakeOff (string applicationToken);
 
-		//		[Static, Export ("options"), Verify ("ObjC method massaged into setter property", "/Users/ghigh/Downloads/TestFlightSDK1.2.6/TestFlight.h", Line = 55)]
-		//		NSDictionary Options { set; }
-		//		[Static, Export ("setOptions:")]
-		//		void SetOptions (string checkpointName);
+		// The values that the dictionary accepts:
+		// TFOptionAttachBacktraceToFeedback => NSNumber.Boolean, Defaults to @NO. Setting to @YES attaches the current backtrace, with symbols, to the feedback.
+		// TFOptionDisableInAppUpdates => NSNumber.Boolean, Defaults to @NO. Setting to @YES, disables the in app update screen shown in BETA apps when there is a new version available on TestFlight.
+		// TFOptionLogToConsole => NSNumber.Boolean, Defaults to @YES. Prints remote logs to Apple System Log.
+		// TFOptionLogToSTDERR => NSNumber.Boolean, Defaults to @YES. Sends remote logs to STDERR when debugger is attached.
+		// TFOptionReinstallCrashHandlers => NSNumber.Boolean, If set to @YES: Reinstalls crash handlers, to be used if a third party library installs crash handlers overtop of the TestFlight Crash Handlers.
+		// TFOptionSendLogOnlyOnCrash => NSNumber.Boolean, Defaults to @NO. Setting to @YES stops remote logs from being sent when sessions end. They would only be sent in the event of a crash.
+		[Static, Export ("setOptions:")]
+		void SetOptions (NSDictionary options);
 
+		/// <summary>
+		/// Track when a user has passed a checkpoint after the flight has taken off. Eg. passed level 1, posted high score
+		/// </summary>
+		/// <param name="checkpointName">The name of the checkpoint, this should be a static string</param>
 		[Static, Export ("passCheckpoint:")]
 		void PassCheckpoint (string checkpointName);
 
-		[Static, Export ("openFeedbackView")]
-		void OpenFeedbackView ();
-
-		//		[Static, Export ("deviceIdentifier"), Verify ("ObjC method massaged into setter property", "/Users/ghigh/Downloads/TestFlightSDK1.2.6/TestFlight.h", Line = 97)]
-		//		string DeviceIdentifier { set; }
-		[Static, Export ("setDeviceIdentifier:")]
-		void SetDeviceIdentifier (string deviceIdentifer);
-
+		/// <summary>
+		/// Submits the feedback to the site, only if feedback is not null or empty.
+		/// </summary>
+		/// <param name="feedback">The feedback your user type in some text field. Or is collected in some other way</param>
 		[Static, Export ("submitFeedback:")]
 		void SubmitFeedback (string feedback);
 
-		[Field ("TFOptionAttachBacktraceToFeedback", "__Internal")]
-		NSString TFOptionAttachBacktraceToFeedback { get; }
+		/// <summary>
+		/// Sets the device identifier.
+		/// DO NOT CALL THIS IN A APPSTORE APP!!!
+		/// Your app won't be processed if you do!
+		/// 
+		/// If you want to use this during testing, you have to call it before TakeOff / ThreadSafeTakeOff
+		/// </summary>
+		/// <param name="deviceIdentifer"> Only use this with the Apple device UDID. DO NOT use Open ID or your own identifier</param>
+		[Obsolete("You cannot use this anymore with Xamarin")]
+		[Static, Export ("setDeviceIdentifier:")]
+		void SetDeviceIdentifier (string deviceIdentifer);
 
-		[Field ("TFOptionDisableInAppUpdates", "__Internal")]
-		NSString TFOptionDisableInAppUpdates { get; }
 
-		[Field ("TFOptionLogToConsole", "__Internal")]
-		NSString TFOptionLogToConsole { get; }
+	}
 
-		[Field ("TFOptionLogToSTDERR", "__Internal")]
-		NSString TFOptionLogToSTDERR { get; }
+	[Static]
+	public interface Options {
 
-		[Field ("TFOptionReinstallCrashHandlers", "__Internal")]
-		NSString TFOptionReinstallCrashHandlers { get; }
+		[Field ("TFOptionDisableInAppUpdates","__Internal")]
+		NSString DisableInAppUpdates { get; }
 
-		[Field ("TFOptionSendLogOnlyOnCrash", "__Internal")]
-		NSString TFOptionSendLogOnlyOnCrash { get; }
+		[Field ("TFOptionFlushSecondsInterval","__Internal")]
+		NSString FlushSecondsInterval { get; }
+
+		[Field ("TFOptionLogOnCheckpoint","__Internal")]
+		NSString LogOnCheckpoint { get; }
+
+		[Field ("TFOptionLogToConsole","__Internal")]
+		NSString LogToConsole { get; }
+
+		[Field ("TFOptionLogToSTDERR","__Internal")]
+		NSString LogToSTDERR { get; }
+
+		[Field ("TFOptionReinstallCrashHandlers","__Internal")]
+		NSString ReinstallCrashHandlers { get; }
+
+		[Field ("TFOptionReportCrashes","__Internal")]
+		NSString ReportCrashes { get; }
+
+		[Field ("TFOptionSendLogOnlyOnCrash","__Internal")]
+		NSString SendLogOnlyOnCrash { get; }
+
+		[Field ("TFOptionSessionKeepAliveTimeout","__Internal")]
+		NSString SessionKeepAliveTimeout { get; }
 	}
 }
